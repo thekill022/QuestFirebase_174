0e26b9d5953c8a979798e8d46749a0400adbceeb
thekill022 <gibfathoni@gmail.com>
2026-01-02 18:01:25 +0700

    membuat composable function HomeBody

--- app/src/main/java/com/example/intro_firebase/view/HalamanHome.kt
+++ app/src/main/java/com/example/intro_firebase/view/HalamanHome.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
@@ -60,5 +61,24 @@ fun HomeScreen(
             retryAction = viewModel::loadSiswa,
             modifier = Modifier.padding(innerPadding).fillMaxSize()
         )
+    }
+}
+
+@Composable
+fun HomeBody(
+    statusUiSiswa: StatusUiSiswa,
+    onSiswaClick : (Int) -> Unit,
+    retryAction : () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        when(statusUiSiswa) {
+            is StatusUiSiswa.Loading -> LoadingScreen()
+            is StatusUiSiswa.Success -> DaftarSiswa(itemSiswa = statusUiSiswa.siswa, onSiswaClick = {onSiswaClick(it.id.toInt)})
+            is StatusUiSiswa.Error -> ErrorScreen(retryAction, modifier = modifier.fillMaxSize())
+        }
     }
 }
