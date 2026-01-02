d74b5a8dccb1084a880c0521abd8902ddb9893e0
thekill022 <gibfathoni@gmail.com>
2026-01-02 16:36:01 +0700

    membuat data class DetailSiswa

--- app/src/main/java/com/example/intro_firebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/intro_firebase/modeldata/Siswa.kt
@@ -6,3 +6,10 @@ data class Siswa (
     val alamat : String = "",
     val telpon : String = ""
 )
+
+data class DetailSiswa (
+    val id : Long = 0,
+    val nama : String = "",
+    val alamat : String = "",
+    val telpon : String = ""
+)
