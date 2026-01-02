a5910fe7b656bae5aa26120d5aef92e463c8dfae
thekill022 <gibfathoni@gmail.com>
2026-01-02 16:35:23 +0700

    membuat data class siswa

---
+++ app/src/main/java/com/example/intro_firebase/modeldata/Siswa.kt
@@ -0,0 +1,8 @@
+package com.example.intro_firebase.modeldata
+
+data class Siswa (
+    val id : Long = 0,
+    val nama : String = "",
+    val alamat : String = "",
+    val telpon : String = ""
+)
