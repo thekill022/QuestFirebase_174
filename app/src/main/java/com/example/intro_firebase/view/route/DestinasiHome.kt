226d06364ede07a25e6d82d95bd6c6bf8ae04a14
thekill022 <gibfathoni@gmail.com>
2026-01-02 17:36:02 +0700

    membuat DestinasiHome

---
+++ app/src/main/java/com/example/intro_firebase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.intro_firebase.view.route
+
+import com.example.intro_firebase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: Int = R.string.app_name
+}
