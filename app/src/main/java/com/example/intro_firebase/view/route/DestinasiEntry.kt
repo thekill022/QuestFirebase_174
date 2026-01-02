61168b2a14c7e9a1c942054224d163a578997de0
thekill022 <gibfathoni@gmail.com>
2026-01-02 17:37:27 +0700

    membuat DestinasiEntry

---
+++ app/src/main/java/com/example/intro_firebase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.intro_firebase.view.route
+
+import com.example.intro_firebase.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route: String = "item_entry"
+    override val titleRes: Int = R.string.entry_siswa
+}
