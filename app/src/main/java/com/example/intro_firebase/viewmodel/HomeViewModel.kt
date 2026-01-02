1c6c2bc08d5648f1dcdfd54011eac302602618e7
thekill022 <gibfathoni@gmail.com>
2026-01-02 17:22:01 +0700

    membuat class HomeViewModel

--- app/src/main/java/com/example/intro_firebase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/intro_firebase/viewmodel/HomeViewModel.kt
@@ -1,9 +1,36 @@
 package com.example.intro_firebase.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.intro_firebase.modeldata.Siswa
+import com.example.intro_firebase.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var statusUiSiswa : StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositorySiswa.getDataSiswa())
+            } catch (e : IOException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+
 }
