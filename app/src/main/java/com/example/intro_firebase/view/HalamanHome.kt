2a779bbd4688ecab997e7411bf9fb23f599e4be1
thekill022 <gibfathoni@gmail.com>
2026-01-02 18:12:02 +0700

    membuat composable function ItemSiswa

--- app/src/main/java/com/example/intro_firebase/view/HalamanHome.kt
+++ app/src/main/java/com/example/intro_firebase/view/HalamanHome.kt
@@ -2,19 +2,27 @@ package com.example.intro_firebase.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -112,9 +120,36 @@ fun DaftarSiswa(
             person ->
             ItemSiswa(
                 siswa = person,
-                modifier= Modifier.padding(dimensionResource(id = R.dimen.padding_small))
+                modifier= Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
                     .clickable { onSiswaClick(person) }
             )
+        }
+    }
+}
+
+@Composable
+fun ItemSiswa(
+    siswa : Siswa,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Text(text = siswa.nama, style = MaterialTheme.typography.titleLarge)
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(imageVector = Icons.Default.Phone, contentDescription = null)
+                Text(text = siswa.telpon, style = MaterialTheme.typography.titleMedium)
+            }
+            Text(text = siswa.alamat, style = MaterialTheme.typography.titleMedium)
         }
     }
 }
