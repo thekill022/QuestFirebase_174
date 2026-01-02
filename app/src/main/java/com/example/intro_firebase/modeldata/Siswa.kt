c135a458b108f26c3e52bcf3bee10eb543b7570c
thekill022 <gibfathoni@gmail.com>
2026-01-02 16:39:54 +0700

    membuat fungsi konversi ke UIStateSiswa untuk siswa

--- app/src/main/java/com/example/intro_firebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/intro_firebase/modeldata/Siswa.kt
@@ -21,3 +21,7 @@ data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid : Boolean = false
 )
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false) : UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid)
