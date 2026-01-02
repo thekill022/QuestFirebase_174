d223b01b4c39eae2c342872b31eb38e05e10c7d1
thekill022 <gibfathoni@gmail.com>
2026-01-02 18:43:43 +0700

    memanggil DataSiswaApp di MainActivity.kt

--- app/src/main/java/com/example/intro_firebase/MainActivity.kt
+++ app/src/main/java/com/example/intro_firebase/MainActivity.kt
@@ -4,25 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.intro_firebase.ui.theme.Intro_firebaseTheme
+import com.example.intro_firebase.view.controllNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Intro_firebaseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) {innerPadding ->
+                    DataSiswaApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
