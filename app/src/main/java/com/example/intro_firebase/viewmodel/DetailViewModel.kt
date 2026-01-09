61a0de730e99cfd686d63bd9beaf0b73b7eaa207
thekill022 <gibfathoni@gmail.com>
2026-01-09 22:48:26 +0700

    menambahkakn sealed class StatusUIDetail di DetailViewModel.kt

--- app/src/main/java/com/example/intro_firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/intro_firebase/viewmodel/DetailViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.intro_firebase.viewmodel
 
+import com.example.intro_firebase.modeldata.Siswa
+
+sealed class StatusUIDetail {
+    data class Success(val satuSiswa : Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
