d5d6ada65ffa645b027e9e967b1adaaad744c262
thekill022 <gibfathoni@gmail.com>
2026-01-09 23:48:24 +0700

    inisialisais detailviewmodel dan editviewmodel di PenyediaViewModel

--- app/src/main/java/com/example/intro_firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/intro_firebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.intro_firebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -17,6 +18,12 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa)
+        }
+        initializer {
+            EditViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa)
         }
     }
 }
