2ed29ad0c0551c28df8ee12735cdc7de8993c7bd
thekill022 <gibfathoni@gmail.com>
2026-01-09 22:35:20 +0700

    menambahkakn method untuk delete data siswa

--- app/src/main/java/com/example/intro_firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/intro_firebase/repositori/RepositorySiswa.kt
@@ -73,4 +73,10 @@ class FirebaseRepositorySiswa : RepositorySiswa {
         ).await()
     }
 
+    override suspend fun hapusSatuSiswa(id: Long) {
+        val docQuery = collection.whereEqualTo("id", id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+        collection.document(docId).delete().await()
+    }
+
 }
