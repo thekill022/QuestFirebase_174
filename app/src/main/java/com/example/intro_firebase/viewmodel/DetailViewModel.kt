f5417e8d21ddd3ddeadf11cb6c78a75ff6dafc6b
thekill022 <gibfathoni@gmail.com>
2026-01-09 22:52:52 +0700

    mengubah sealed class menjadi sealed interface

--- app/src/main/java/com/example/intro_firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/intro_firebase/viewmodel/DetailViewModel.kt
@@ -2,7 +2,7 @@ package com.example.intro_firebase.viewmodel
 
 import com.example.intro_firebase.modeldata.Siswa
 
-sealed class StatusUIDetail {
+sealed interface StatusUIDetail {
     data class Success(val satuSiswa : Siswa?) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
