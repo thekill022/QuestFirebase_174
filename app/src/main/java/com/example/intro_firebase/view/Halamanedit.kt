caa0d82fb5d5921c95b211104a1f7ea863fc6d93
thekill022 <gibfathoni@gmail.com>
2026-01-10 00:29:45 +0700

    membuat composable function EditSiswaScreen

--- app/src/main/java/com/example/intro_firebase/view/Halamanedit.kt
+++ app/src/main/java/com/example/intro_firebase/view/Halamanedit.kt
@@ -1,2 +1,45 @@
 package com.example.intro_firebase.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.intro_firebase.viewmodel.EditViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.intro_firebase.view.route.DestinasiEdit
+import com.example.intro_firebase.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack : () -> Unit,
+    onNavigateUp : () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(id = DestinasiEdit.titleRes),
+                canNavigateback = true,
+                navigateUp = onNavigateUp)
+        },
+        modifier = modifier
+    ) {innerPad ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPad))
+    }
+}
