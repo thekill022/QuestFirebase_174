7fd3de9dbb3481a18ecb9c9b811a3f37ed1e5b9a
thekill022 <gibfathoni@gmail.com>
2026-01-09 22:25:54 +0700

    menambahkakn method untuk get siswa by id

--- app/src/main/java/com/example/intro_firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/intro_firebase/repositori/RepositorySiswa.kt
@@ -43,4 +43,21 @@ class FirebaseRepositorySiswa : RepositorySiswa {
         docRef.set(data).await()
     }
 
+    override suspend fun getSatuSiswa(id: Long): Siswa? {
+        return try {
+            val query = collection.whereEqualTo("id", id).get().await()
+            query.documents.firstOrNull()?.let { doc ->
+                Siswa(
+                    id = doc.getLong("id")?.toLong() ?: 0,
+                    nama = doc.getString("nama") ?: "",
+                    alamat = doc.getString("alamat") ?: "",
+                    telpon = doc.getString("telpon") ?: ""
+                )
+            }
+        } catch (e : Exception) {
+            println("Gagal baca data siswa : ${e.message}")
+            null
+        }
+    }
+
 }
