d8e2460b5d4d447b4c42e458567311bcce3915aa
thekill022 <gibfathoni@gmail.com>
2026-01-09 23:41:07 +0700

    deklarasi variable untuk uistate dan id dari siswa

--- app/src/main/java/com/example/intro_firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/intro_firebase/viewmodel/EditViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.intro_firebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.intro_firebase.modeldata.UIStateSiswa
+import com.example.intro_firebase.modeldata.toUiStateSiswa
+import com.example.intro_firebase.repositori.RepositorySiswa
+import com.example.intro_firebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel (savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    private val idSiswa : Long = savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong() ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!.toUiStateSiswa(true)
+        }
+    }
+}
