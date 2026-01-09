c624cc8110f6b7f2b58ff5011a0147079a507caa
thekill022 <gibfathoni@gmail.com>
2026-01-09 22:17:10 +0700

    menambakan method baru di RepositorySiswa

--- app/src/main/java/com/example/intro_firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/intro_firebase/repositori/RepositorySiswa.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.tasks.await
 interface RepositorySiswa {
     suspend fun getDataSiswa() : List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
+    suspend fun getSatuSiswa(id : Long) : Siswa?
+    suspend fun editSatuSiswa(id : Long, siswa: Siswa)
+    suspend fun hapusSatuSiswa(id : Long)
 }
 
 class FirebaseRepositorySiswa : RepositorySiswa {
